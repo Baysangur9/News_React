8dbcdc89ccf1d488b226b3205698f7137f76fa62
Baysangur Elbiev <Nepstor95@gmail.com>
2024-03-09 14:53:47 +0300

    add pagination, change api endpoint

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -4,23 +4,43 @@ import styles from "../../styles/main.module.css";
 import {getNews} from "../../api/apiNews";
 import NewsList from "../../components/NewsList/NewsList";
 import Skeleton from "../../components/Skeleton/Skeleton";
+import Pagination from "../../components/Pagination/Pagination";
 
 const Main = () => {
   const [news, setNews] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = 10;
+  const pageSize = 10;
   useEffect(() => {
-    const fetchNews = async () => {
+    const fetchNews = async (currentPage) => {
       try {
         setIsLoading(true);
-        const responce = await getNews();
+        const responce = await getNews(currentPage, pageSize);
         setNews(responce.news);
         setIsLoading(false);
       } catch (error) {
         console.log(error);
       }
     };
-    fetchNews();
-  }, []);
+    fetchNews(currentPage);
+  }, [currentPage]);
+
+  const hundleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const hundlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const hundlePageClic = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
 
   return (
     <main className={styles.main}>
@@ -29,12 +49,27 @@ const Main = () => {
       ) : (
         <Skeleton type={"banner"} count={1} />
       )}
-
+      <Pagination
+        totalPages={totalPages}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        hundleNextPage={hundleNextPage}
+        hundlePrevPage={hundlePrevPage}
+        hundlePageClic={hundlePageClic}
+      />
       {!isLoading ? (
         <NewsList news={news} />
       ) : (
         <Skeleton type={"item"} count={10} />
       )}
+      <Pagination
+        totalPages={totalPages}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        hundleNextPage={hundleNextPage}
+        hundlePrevPage={hundlePrevPage}
+        hundlePageClic={hundlePageClic}
+      />
     </main>
   );
 };
