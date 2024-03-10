b339eac41e3067ed6bb8b37ecc9e4415667e3fad
Baysangur Elbiev <Nepstor95@gmail.com>
2024-03-10 14:59:17 +0300

    refactoring

---
+++ src/helpers/hocs/withSkeleton.jsx
@@ -0,0 +1,14 @@
+import Skeleton from "../../components/Skeleton/Skeleton";
+
+function withSkeleton(Component, type, count) {
+  return function withSkeleton(props) {
+    const {isLoading, ...restProps} = props;
+    if (isLoading) {
+      return <Skeleton count={count} type={type} />;
+    }
+
+    return <Component {...restProps} />;
+  };
+}
+
+export default withSkeleton;
